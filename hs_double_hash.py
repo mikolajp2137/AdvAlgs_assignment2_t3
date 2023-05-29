7bea77cd50799f4703b658238ebcecd8425ee2b3
Mikolaj <pienkoszmikolaj@gmail.com>
2023-05-29 19:06:42 +0200

    minor fixes

--- hs_double_hash.py
+++ hs_double_hash.py
@@ -12,8 +12,21 @@ class HashTable:
         else:
             return key % self.size
 
+    def second_hash_function(self, key):
+        h = 0
+        if isinstance(key, str):
+            # Use a combination of polynomial rolling hash and XOR operations
+            for char in key:
+                h ^= (h << 5) + (h >> 2) + ord(char)
+        else:
+            # Use XOR and bit shifting for numeric keys
+            for num in key:
+                h ^= (h << 5) + (h >> 2) + num
+
+        return h % self.size
+
     def double_hash(self, key, i):
-        return (self.hash_function(key) + i) % self.size
+        return (self.hash_function(key) + i * 3 + self.second_hash_function(key)) % self.size
 
     def insert(self, key, value):
         i = 0
@@ -26,7 +39,7 @@ class HashTable:
             total_slots += 1
 
             if total_slots == self.size:
-                raise Exception("Hash table is full. Resize the table.")
+                raise Exception("full")
 
         self.table[hash_value] = (key, value)
 
--- main.py
+++ main.py
@@ -1,49 +1,85 @@
+import random
+import string
 import timeit
-import hs_double_hash, hs_linear_probing, hs_separate_chaining
-import random, string
+import matplotlib.pyplot as plt
+import hs_double_hash
+import hs_linear_probing
+import hs_separate_chaining
 
-times = []
-length = 900000
-keysG = lambda: [''.join(random.choices(string.ascii_letters, k=20)) for _ in range(length)]
-valuesG = lambda: [random.randint(1, 9) for _ in range(length)]
+def insert_elements(hash_table, keys, values, fill_percentage):
+    length = int(len(keys) * fill_percentage)
+    for i in range(length):
+        hash_table.insert(keys[i], values[i])
 
-keys, values = keysG(), valuesG()
+def search_element(hash_table, key):
+    return hash_table.search(key)
 
-# DOUBLE HASHING
-DH_hash_table = hs_double_hash.HashTable(1000000)
+def generate_data(length):
+    keys = [''.join(random.choices(string.ascii_letters, k=20)) for _ in range(length)]
+    values = [random.randint(1, 9) for _ in range(length)]
+    return keys, values
 
+def measure_insertion_time(HashTable, keys, values, fill_percentages):
+    insertion_times = []
+    for fill_percentage in fill_percentages:
+        hash_table = HashTable(1000000)  # Create a new hash table instance
+        insert_func = lambda: insert_elements(hash_table, keys, values, fill_percentage)
+        insertion_time = timeit.timeit(insert_func, number=1)
+        insertion_times.append(insertion_time)
+    return insertion_times
 
-def DH_insert():
-    DH_hash_table.insert("apple", 1)
-    for v in range(length):
-        DH_hash_table.insert(keys[v], values[v])
+def measure_search_time(HashTable, keys, values, key_to_search):
+    search_times = []
+    for fill_percentage in fill_percentages:
+        hash_table = HashTable(1000000)  # Create a new hash table instance
+        insert_elements(hash_table, keys, values, fill_percentage)  # Insert elements before searching
+        search_func = lambda: search_element(hash_table, key_to_search)
+        search_time = timeit.timeit(search_func, number=1)
+        search_times.append(search_time)
+    return search_times
 
+def plot_insertion_times(fill_percentages, DH_times, LP_times, SC_times):
+    plt.plot(fill_percentages, DH_times, marker='o', label='Double Hashing')
+    plt.plot(fill_percentages, LP_times, marker='o', label='Linear Probing')
+    plt.plot(fill_percentages, SC_times, marker='o', label='Separate Chaining')
+    plt.xlabel('Fill Percentage')
+    plt.ylabel('Insertion Time (seconds)')
+    plt.title('Insertion Time vs. Fill Percentage')
+    plt.legend()
+    plt.savefig('insertion_times.png')
+    plt.show()
 
-DHtime = timeit.timeit(lambda: DH_insert(), number=1)
-print("Time", DHtime)
+def plot_search_times(fill_percentages, DH_times, LP_times, SC_times):
+    plt.plot(fill_percentages, DH_times, marker='o', label='Double Hashing')
+    plt.plot(fill_percentages, LP_times, marker='o', label='Linear Probing')
+    plt.plot(fill_percentages, SC_times, marker='o', label='Separate Chaining')
+    plt.xlabel('Fill Percentage')
+    plt.ylabel('Search Time (seconds)')
+    plt.title('Search Time for Key "test"')
+    plt.legend()
+    plt.savefig('search_times.png')
+    plt.show()
 
-# LINEAR PROBING
-# LP_hash_table = hs_linear_probing.HashTable(1000000)
-#
-#
-# def LP_insert():
-#     LP_hash_table.insert("apple", 1)
-#     for v in range(length):
-#         LP_hash_table.insert(keys[v], values[v])
-#
-#
-# LPtime = timeit.timeit(lambda: LP_insert(), number=1)
-# print("Time", LPtime)
+# Generate data
+length = 1000000
+keys, values = generate_data(length)
 
-# SEPARATE CHAINING
-# SC_hash_table = hs_separate_chaining.HashTable(1000000)
-#
-#
-# def SC_insert():
-#     SC_hash_table.insert("apple", 1)
-#     for v in range(length):
-#         SC_hash_table.insert(keys[v], values[v])
-#
-#
-# SCtime = timeit.timeit(lambda: SC_insert(), number=1)
-# print("Time", SCtime)
+# Fill Percentage Values
+fill_percentages = [0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+# Measure insertion times
+DH_insertion_times = measure_insertion_time(hs_double_hash.HashTable, keys, values, fill_percentages)
+LP_insertion_times = measure_insertion_time(hs_linear_probing.HashTable, keys, values, fill_percentages)
+SC_insertion_times = measure_insertion_time(hs_separate_chaining.HashTable, keys, values, fill_percentages)
+
+# Plot insertion times
+plot_insertion_times(fill_percentages, DH_insertion_times, LP_insertion_times, SC_insertion_times)
+
+# Measure search times for key "test"
+key_to_search = "test"
+DH_search_times = measure_search_time(hs_double_hash.HashTable, keys, values, key_to_search)
+LP_search_times = measure_search_time(hs_linear_probing.HashTable, keys, values, key_to_search)
+SC_search_times = measure_search_time(hs_separate_chaining.HashTable, keys, values, key_to_search)
+
+# Plot search times
+plot_search_times(fill_percentages, DH_search_times, LP_search_times, SC_search_times)
